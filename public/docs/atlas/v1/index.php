096028deb9a0f98134a66e6d026f3b014cded6ec
xnebulr <prime@nebulr.org>
2019-11-23 17:06:36 +0100

    fix favicon

--- public/docs/atlas/v1/index.php
+++ public/docs/atlas/v1/index.php
@@ -6,14 +6,14 @@
     <link rel="stylesheet" type="text/css" href="../assets/css/base.css" >
     <link rel="stylesheet" type="text/css" href="../assets/css/swagger-ui.css" >
 
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/apple-touch-icon.png' ?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/favicon-32x32.png' ?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/favicon-16x16.png' ?>">
-    <link rel="manifest" href="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/site.webmanifest' ?>">
-    <link rel="mask-icon" href="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/safari-pinned-tab.svg' ?>" color="#e14748">
-    <link rel="shortcut icon" href="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/favicon.ico' ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#e14748">
+    <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico">
     <meta name="msapplication-TileColor" content="#e14748">
-    <meta name="msapplication-config" content="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/img/favicon/browserconfig.xml' ?>">
+    <meta name="msapplication-config" content="/assets/img/favicon/browserconfig.xml">
     <meta name="theme-color" content="#e14748">
   </head>
 
