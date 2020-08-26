ca0e2b714e42a1d6e3693ab12f2a8e99ad11726b
Fabian <prime@nebulr.org>
2020-08-26 21:58:24 +0200

    update dependencies

--- src/v1/classes/Version.php
+++ src/v1/classes/Version.php
@@ -10,13 +10,13 @@ class Version
     {
         require_once __DIR__ . '/../../../vendor/autoload.php';
 
-        $dotenv = Dotenv::create(__DIR__ . '/../../../');
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
         $dotenv->load();
 
-        $db_host = getenv('DB_HOST');
-        $db_name = getenv('DB_NAME');
-        $db_user = getenv('DB_USER');
-        $db_pass = getenv('DB_PASS');
+        $db_host = $_ENV['DB_HOST'];
+        $db_name = $_ENV['DB_NAME'];
+        $db_user = $_ENV['DB_USER'];
+        $db_pass = $_ENV['DB_PASS'];
 
         $this->conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
         if (!$this->conn) {
